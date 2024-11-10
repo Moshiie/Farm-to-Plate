be7b55f67757920a7cd81935028b9730a9d301f2
Moshiie <136818985+Moshiie@users.noreply.github.com>
2024-11-11 00:05:05 +0800

    JC ikaw na
    Ikaw na ani JC

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto'; // Required for React Native compatibility
 
 // Replace with your actual Supabase URL and public anon key
-const SUPABASE_URL = 'https://tegzicopvmcskrsjutln.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZ3ppY29wdm1jc2tyc2p1dGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMjM4MzUsImV4cCI6MjA0Njc5OTgzNX0.w40mx3v7u7UEWZxNAWKbFEy8O_3gAMezfUmUsGqwKKQ';
+const SUPABASE_URL = 'JC ikaw na bahala ani';
+const SUPABASE_KEY = 'apil sad ni';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
